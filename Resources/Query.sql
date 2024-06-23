fc857ae098864b0e428c9d67ccfa9661ce8ef1bb
Kayzel <kapitonovroman5@gmail.com>
2024-06-23 20:36:44 +0300

    move folders

---
+++ Resources/Query.sql
@@ -0,0 +1,9 @@
+﻿-- Products: ProductId, ProductName
+-- Categories: CategoryID, CategoryName
+-- ProductCategories: ProductID, CategoryID
+
+SELECT p.ProductName, c.CategoryName
+FROM Products p
+         LEFT JOIN ProductCategories pc ON p.ProductID = pc.ProductID
+         LEFT JOIN Categories c ON pc.CategoryID = c.CategoryID
+ORDER BY p.ProductName, c.CategoryName;
